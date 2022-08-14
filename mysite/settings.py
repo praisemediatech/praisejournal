ec9179684b97cb126c563fa2f23a90ebc2939e35
praisemediatech <praisetechmedia@gmail.com>
2022-08-14 23:14:50 +0100

    Fixed database and taglist template

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,24 +86,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'praisedb',
-        'USER': 'praise',
-        'PASSWORD': 'praisemedia08141772672',
-        'HOST': 'localhost',
-        'POST': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'praisedb',
+#         'USER': 'praise',
+#         'PASSWORD': 'praisemedia08141772672',
+#         'HOST': 'localhost',
+#         'POST': '',
+#     }
+# }
 
 
 # Password validation
